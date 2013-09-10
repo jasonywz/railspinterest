3bdc5dd1d7803bc015faff2380d6aac887b388c3
Jason <jsnywz@gmail.com>
2013-09-09 23:52:16 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railspinterest::Application.config.secret_key_base = '63ec4134c627f5e8d3af47fbd8f26dc70c1dc4da1860248616f3585b09cafdc85f609f83263efff7955d1babd3430718e19ae640b5a7b9723f0d389e6f335dd7'
